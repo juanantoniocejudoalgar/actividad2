e811d1a7869e7aa4ea531e4e6a278671d13db692
Juan Antonio <juanantoniocejudoalgar@gmail.com>
2024-10-28 13:46:17 +0100

    ej1upload
    Subida del primer ejercicio funcionando

---
+++ Javascript/ejercicio1/ej1.js
@@ -0,0 +1,12 @@
+
+
+/* 
+
+Ejercicio 1 
+Creamos un archivo externo donde insertamos el código js que deseamos ejecutar.
+
+
+*/
+
+console.log("Hola Mundo!");
+console.log("Soy el primer script");
